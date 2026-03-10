ad88efcd6f306b8f0e318443baf65da4acecebe3
grant <ruddickmg@gmail.com>
2026-03-10 10:29:01 -1000

    update connect function

--- src/acp/connection/manager.rs
+++ src/acp/connection/manager.rs
@@ -1,6 +1,6 @@
-use crate::acp::connection::{Connection, stdio};
+use crate::acp::connection::{stdio, Connection};
 use crate::nvim::autocommands::ResponseHandler;
-use crate::{Handler, acp::error::Error};
+use crate::{acp::error::Error, Handler};
 use agent_client_protocol::{Client, Implementation, InitializeRequest, ProtocolVersion};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
