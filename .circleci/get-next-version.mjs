a47d0f010f829287716bef9f781a57e1b6376348
Marcus Ruddick <Ruddickmg@gmail.com>
2026-03-25 22:34:40 -1000

    chore: debug version tool

--- .circleci/get-next-version.mjs
+++ .circleci/get-next-version.mjs
@@ -1,15 +1,21 @@
 import semanticRelease from 'semantic-release';
 
 try {
+  console.log("running semantic release");
   const result = await semanticRelease({ dryRun: true });
+  console.log("result", result);
   const version = result?.nextRelease?.version;
+  console.log("retreived version!", version);
 
   if (version) {
+    console.log("returning", version);
     process.stdout.write(version);
   } else {
+    console.log("there was no version!");
     process.stderr.write('No next release version determined by semantic-release.\n');
   }
 } catch (err) {
+  console.log("an error happened", err);
   process.stderr.write(`semantic-release error: ${err.message}\n`);
   process.exit(1);
 }
