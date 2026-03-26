cdf489c15d7fb488a5f66d9463654ee4565fd373
Marcus Ruddick <Ruddickmg@gmail.com>
2026-03-25 22:56:59 -1000

    fix: suppress console logs in semantic release script
    Updated semantic release logging to suppress output.

--- .circleci/get-next-version.mjs
+++ .circleci/get-next-version.mjs
@@ -1,21 +1,24 @@
 import semanticRelease from 'semantic-release';
 
 try {
-  console.log("running semantic release");
-  const result = await semanticRelease({ dryRun: true });
-  console.log("result", result);
+  const result = await semanticRelease({
+    dryRun: true,
+    logger: {
+      log: () => {},
+      error: () => {},
+      success: () => {},
+      warn: () => {}
+    }
+  });
   const version = result?.nextRelease?.version;
-  console.log("retreived version!", version);
 
   if (version) {
-    console.log("returning", version);
     process.stdout.write(version);
   } else {
     console.log("there was no version!");
     process.stderr.write('No next release version determined by semantic-release.\n');
   }
 } catch (err) {
-  console.log("an error happened", err);
   process.stderr.write(`semantic-release error: ${err.message}\n`);
   process.exit(1);
 }
